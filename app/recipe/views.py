f3a2e69b369524cbcc6f99c7cdd9a9ab94de5c4c
MakaronToja2 <wloch.jakub2@gmail.com>
2024-03-04 15:44:04 +0100

    Views and Tests for destroying,updating Tags

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -35,8 +35,11 @@ class RecipeViewSet(viewsets.ModelViewSet):
         """Create a new recipe"""
         serializer.save(user=self.request.user)
 
-class TagViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
-    """Manage tagg in DB"""
+class TagViewSet(mixins.DestroyModelMixin,
+                 mixins.UpdateModelMixin,
+                 mixins.ListModelMixin,
+                 viewsets.GenericViewSet):
+    """Manage tags in DB"""
     serializer_class = serializers.TagSerializer
     queryset = Tag.objects.all()
     authentication_classes = [TokenAuthentication]
@@ -45,3 +48,4 @@ class TagViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
     def get_queryset(self):
         """Filter queryset for authenticated user"""
         return self.queryset.filter(user=self.request.user).order_by('-name')
+

aea2055cde8b085a29177960109d16576023dcba
MakaronToja2 <wloch.jakub2@gmail.com>
2024-05-08 17:24:24 +0200

    Final changes to deployment

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -11,7 +11,7 @@ from django.core.management.base import BaseCommand
 
 class Command(BaseCommand):
     """Django command to wait for the DB"""
-    def handle(self, *args, **kwargs):
+    def handle(self, *args, **options):
         """Entrypoint for command"""
         self.stdout.write('Waiting for the database...')
         db_up = False
